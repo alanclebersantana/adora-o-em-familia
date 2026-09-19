fb754ee5c0435f3c42778c1c058ec5fb702c2469
Alan <alanclebersantana@gmail.com>
2026-09-19 18:25:26 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Alan & Átina — service worker. Troque a versão a cada publicação. */
-const VERSION = 'aa-v1';
+const VERSION = 'aa-v2';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
 self.addEventListener('install', event => {
